923830d11894c04932cd5a2d8bbc47f6650bf755
himawanAd <himawan.ad@student.uns.ac.id>
2025-03-17 03:38:52 +0700

    tambah table db monitoring_log

--- mod/monitoring/db/upgrade.php
+++ mod/monitoring/db/upgrade.php
@@ -32,5 +32,26 @@ function xmldb_monitoring_upgrade($oldversion) {
         upgrade_mod_savepoint(true, 2025021300, 'monitoring');
     }
 
+    if ($oldversion < 20250311001) { 
+        $table = new xmldb_table('monitoring_log');
+
+        $table->add_field('id', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, XMLDB_SEQUENCE, null);
+        $table->add_field('student_id', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, null);
+        $table->add_field('course_module_id', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, null);
+        $table->add_field('status', XMLDB_TYPE_CHAR, '20', null, XMLDB_NOTNULL, null, null);
+        $table->add_field('logged_at', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, null);
+
+        $table->add_key('primary', XMLDB_KEY_PRIMARY, ['id']);
+
+        $table->add_key('student_fk', XMLDB_KEY_FOREIGN, ['student_id'], 'user', ['id']);
+        $table->add_key('course_module_fk', XMLDB_KEY_FOREIGN, ['course_module_id'], 'course_modules', ['id']);
+
+        if (!$dbman->table_exists($table)) {
+            $dbman->create_table($table);
+        }
+
+        upgrade_mod_savepoint(true, 2025031101, 'monitoring');
+    }
+
     return true;
 }
--- mod/monitoring/version.php
+++ mod/monitoring/version.php
@@ -2,7 +2,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_monitoring';  // Nama komponen plugin
-$plugin->version   = 2025021300;        // Versi plugin (YYYYMMDDXX)
+$plugin->version   = 2025031101;        // Versi plugin (YYYYMMDDXX)
 $plugin->requires  = 2024100100;        // Moodle versi minimum yang dibutuhkan
 $plugin->maturity  = MATURITY_ALPHA;
 $plugin->release   = 'v1.0';
